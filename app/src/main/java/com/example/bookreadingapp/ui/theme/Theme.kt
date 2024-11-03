3e6b87f798648692b4b79261ad2e15e5aa6d6304
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-03 01:41:47 -0500

    Change the shape of the Card

--- app/src/main/java/com/example/bookreadingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.view.View
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -107,6 +108,7 @@ fun BookReadingAppTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
