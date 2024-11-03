89e3e0e1afdeb42917414478eaef29577e38a3ae
David-Parkerr <davidparker0040@gmail.com>
2024-11-02 22:16:53 -0400

    Add window-size-class dependency and implement in main component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("androidx.navigation:navigation-compose:2.5.3")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("androidx.compose.material3:material3-window-size-class")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/bookreadingapp/MainActivity.kt
+++ app/src/main/java/com/example/bookreadingapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.bookreadingapp
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,20 +20,39 @@ import com.example.bookreadingapp.objects.NavigationHost
 import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterial3WindowSizeClassApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BookReadingAppTheme {
-                BookReadingApp()
+                val windowSize = calculateWindowSizeClass(this)
+                BookReadingApp(
+                    windowSize = windowSize.widthSizeClass
+                )
             }
         }
     }
 }
 
 @Composable
-fun BookReadingApp()  {
+fun BookReadingApp(windowSize: WindowWidthSizeClass, modifier: Modifier = Modifier)  {
     val navController = rememberNavController()
+
+    val adaptiveNavigationType = when (windowSize) {
+        WindowWidthSizeClass.Compact -> {
+            // Screen State
+        }
+        WindowWidthSizeClass.Medium -> {
+            // Screen State
+        }
+        WindowWidthSizeClass.Expanded -> {
+            // Screen State
+        }
+        else -> {
+            // Screen State
+        }
+    }
 
     Scaffold(
         content = {padding ->
@@ -46,6 +69,8 @@ fun BookReadingApp()  {
 @Composable
 fun GreetingPreview() {
     BookReadingAppTheme {
-        BookReadingApp()
+        BookReadingApp(
+            windowSize = WindowWidthSizeClass.Compact
+        )
     }
 }
