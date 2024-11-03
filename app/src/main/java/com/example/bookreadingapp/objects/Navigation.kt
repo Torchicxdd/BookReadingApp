5681d3811fb1084f8a3e38ab4980ef4da8d5d955
David-Parkerr <davidparker0040@gmail.com>
2024-11-03 03:45:44 -0500

    Create NavRail component layout

--- app/src/main/java/com/example/bookreadingapp/objects/Navigation.kt
+++ app/src/main/java/com/example/bookreadingapp/objects/Navigation.kt
@@ -1,8 +1,12 @@
 package com.example.bookreadingapp.objects
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -47,41 +51,68 @@ fun NavigationHost(
 
 @Composable
 fun BottomNavBar(
-    navController: NavHostController
+    navController: NavHostController,
+    modifier: Modifier = Modifier
 ) {
     NavigationBar {
         val backStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = backStackEntry?.destination?.route
-        NavBarItems.BarItems.forEach{
-                navItem -> NavigationBarItem(
-            selected = currentRoute == navItem.route,
-            onClick = {
-                navController.navigate(navItem.route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
+        NavBarItems.BarItems.forEach{ navItem ->
+            NavigationBarItem(
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            },
-            icon = {
-                Icon(imageVector = navItem.image, contentDescription = navItem.title)
-            },
-            label = { Text(text = navItem.title) }
-        )
+                },
+                icon = {
+                    Icon(imageVector = navItem.image, contentDescription = navItem.title)
+                },
+                label = { Text(text = navItem.title) }
+            )
         }
     }
 }
 
 @Composable
 fun NavRail(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    // Some Composable
+    NavigationBar {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+        NavigationRail {
+            Spacer(Modifier.weight(1f))
+            NavBarItems.BarItems.forEach { navItem ->
+                NavigationRailItem(
+                    selected = currentRoute == navItem.route,
+                    onClick = {
+                        navController.navigate(navItem.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(imageVector = navItem.image, contentDescription = navItem.title)
+                    },
+                )
+            }
+            Spacer(Modifier.weight(1f))
+        }
+    }
 }
 
 @Composable
 fun PermanentNavDrawer(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     // Some Composable
