980b8723ee9a2c9c8e12994ff5f101dc8cbebe36
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-02 21:45:51 -0400

    Fix titles in bottom bar

--- app/src/main/java/com/example/bookreadingapp/objects/BarItem.kt
+++ app/src/main/java/com/example/bookreadingapp/objects/BarItem.kt
@@ -1,8 +1,5 @@
-package com.example.bookreadingapp.objects
-
+import android.content.Context
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
@@ -18,31 +15,45 @@ data class BarItem(
 )
 
 object NavBarItems {
-    val  BarItems = listOf(
-        BarItem(
-            title = R.string.home_icon_title.toString(),
-            image = Icons.Filled.Home,
-            route = "home"
-        ),
-        BarItem(
-            title = R.string.library_icon_title.toString(),
-            image = Icons.Filled.MoreVert,
-            route = "library"
-        ),
-        BarItem(
-            title = R.string.search_icon_title.toString(),
-            image = Icons.Filled.Search,
-            route = "search"
-        ),
-        BarItem(
-            title = R.string.table_content_icon_title.toString(),
-            image = Icons.Filled.Menu,
-            route = "content_table"
-        ),
-        BarItem(
-            title = R.string.reading_mode_icon_title.toString(),
-            image = Icons.Filled.PlayArrow,
-            route = "reading"
+    fun getBarItems(context: Context): List<BarItem> {
+        val items = mutableListOf<BarItem>()
+
+        items.add(
+            BarItem(
+                title = context.getString(R.string.home_icon_title),
+                image = Icons.Filled.Home,
+                route = "home"
+            )
         )
-    )
+        items.add(
+            BarItem(
+                title = context.getString(R.string.library_icon_title),
+                image = Icons.Filled.MoreVert,
+                route = "library"
+            )
+        )
+        items.add(
+            BarItem(
+                title = context.getString(R.string.search_icon_title),
+                image = Icons.Filled.Search,
+                route = "search"
+            )
+        )
+        items.add(
+            BarItem(
+                title = context.getString(R.string.table_content_icon_title),
+                image = Icons.Filled.Menu,
+                route = "content_table"
+            )
+        )
+        items.add(
+            BarItem(
+                title = context.getString(R.string.reading_mode_icon_title),
+                image = Icons.Filled.PlayArrow,
+                route = "reading"
+            )
+        )
+
+        return items
+    }
 }
