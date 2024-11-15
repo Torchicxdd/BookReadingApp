f8cc5508bb45ee38e01d2619319e540eeb721ae8
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-15 11:31:44 -0800

    Add constructor

--- app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
+++ app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
@@ -8,6 +8,8 @@ class Books {
 
     constructor()
     constructor(title: String, author: String, coverImage: String) {
-        
+        this.title = title
+        this.author = author
+        this.coverImage = coverImage
     }
 }
