b94c586aed0b27e644684321676abfdffe0ebd33
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-15 11:44:54 -0800

    Made Books class

--- app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
+++ app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
@@ -1,9 +1,19 @@
 package com.example.bookreadingapp.data.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="books")
 class Books {
+    @PrimaryKey(autoGenerate=true)
+    @ColumnInfo(name="bookId")
     var id: Int = 0
+    @ColumnInfo(name="title")
     var title: String = ""
+    @ColumnInfo(name="author")
     var author: String = ""
+    @ColumnInfo(name="coverImage")
     var coverImage: String = ""
 
     constructor()
