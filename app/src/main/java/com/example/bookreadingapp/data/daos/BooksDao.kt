983f0e9014a99a8453e7274e09e1fd8a3b4713bb
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-15 12:09:13 -0800

    Made DAO for Books entity

---
+++ app/src/main/java/com/example/bookreadingapp/data/daos/BooksDao.kt
@@ -0,0 +1,28 @@
+package com.example.bookreadingapp.data.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.bookreadingapp.data.entities.Books
+
+@Dao
+interface BooksDao {
+    @Insert
+    fun insertBook(book: Books)
+
+    @Query("SELECT * FROM books")
+    fun getAttBooks(): LiveData<List<Books>>
+
+    @Query("SELECT * FROM books WHERE bookId=:id")
+    fun findBookById(id: Int): List<Books>
+
+    @Query("SELECT * FROM books WHERE title=:title")
+    fun findBookByName(title: String): List<Books>
+
+    @Query("SELECT * FROM books WHERE title=:title AND author=:author")
+    fun findBookByNameAndAuthor(title: String, author: String): List<Books>
+
+    @Query("DELETE FROM books WHERE bookId=:id")
+    fun deleteBook(id: Int)
+}
