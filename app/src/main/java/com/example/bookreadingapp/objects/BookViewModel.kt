9ffb85f8ee9e3c0bde9db11c37e8c94f66fd097f
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-15 21:31:30 -0500

    Made base viewmodel for books entity

---
+++ app/src/main/java/com/example/bookreadingapp/objects/BookViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.bookreadingapp.objects
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bookreadingapp.data.entities.Books
+import com.example.bookreadingapp.data.repositories.BooksRepository
+
+class BookViewModel(application: Application) : ViewModel() {
+    val allBooks: LiveData<List<Books>>
+    private val repository: BooksRepository
+    val searchResults: MutableLiveData<List<Books>>
+
+    init {
+        val bookDb = BookaAppRoomDatabase.getInstance(application)
+        val booksDao = bookDb.booksDb()
+        repository = BooksRepository(booksDao)
+        allBooks = repository.allBooks
+        searchResults = repository.searchResults
+    }
+
+    fun insertBook(book: Books) {
+        repository.insertBook(book)
+    }
+
+    fun deleteBook(id: Int) {
+        repository.deleteBook(id)
+    }
+
+    fun findBookById(id: Int) {
+        repository.findBookById(id)
+    }
+
+    fun findBookByName(title: String) {
+        repository.findBookByName(title)
+    }
+
+    fun findBookByNameAndAuthor(title: String, author: String) {
+        repository.findBookByNameAndAuthor(title, author)
+    }
+}
