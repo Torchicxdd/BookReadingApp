9cfc728d4ded0e4fcbeefe58232839d4689b240e
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-17 12:11:20 -0500

    Add all strings

--- app/src/main/java/com/example/bookreadingapp/screens/Home.kt
+++ app/src/main/java/com/example/bookreadingapp/screens/Home.kt
@@ -1,6 +1,10 @@
 package com.example.bookreadingapp.screens
 
 import android.content.Context
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.example.bookreadingapp.R
@@ -9,11 +13,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.bookreadingapp.data.Book
 import com.example.bookreadingapp.objects.AppViewModel
 import com.example.bookreadingapp.utils.AdaptiveNavigationType
 
@@ -34,6 +42,86 @@ fun Home(
                 .fillMaxWidth()
         ) {
             Text(text = context.getString(R.string.home), style = MaterialTheme.typography.displayLarge)
+        }
+    }
+}
+
+@Composable
+fun HomeContent(
+    modifier: Modifier = Modifier,
+    context: Context
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
+            Description(context)
+
+            HowToUse(context)
+        }
+    }
+}
+
+@Composable
+fun Description(
+    context: Context
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = context.getString(R.string.welcome_message),
+            style = MaterialTheme.typography.displayMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium))
+        )
+
+        // App description
+        val descriptionArray = context.resources.getStringArray(R.array.app_description)
+        descriptionArray.forEach { descriptionItem ->
+            Text(
+                text = descriptionItem,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+fun HowToUse(
+    context: Context
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = context.getString(R.string.welcome_message),
+            style = MaterialTheme.typography.displayMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium))
+        )
+
+        // "How to Use" steps
+        val howToUseStepsArray = context.resources.getStringArray(R.array.how_to_use_steps)
+        howToUseStepsArray.forEachIndexed { index, step ->
+            Text(
+                text = "${index + 1}. $step",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_small))
+            )
         }
     }
 }
