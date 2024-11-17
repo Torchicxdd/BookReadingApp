e7e0471dfef3bfde23a1de2947e5f40430e4e561
elianasmadja <eliana.smadja@dawsoncollege.qc.ca>
2024-11-17 15:03:00 -0500

    fix merge conflict

--- app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
@@ -8,6 +8,7 @@ class AppViewModelFactory(val application: Application): ViewModelProvider.Facto
     override fun <T: ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(BookViewModel::class.java) -> BookViewModel(application) as T
+            modelClass.isAssignableFrom(ChapterViewModel::class.java) -> ChapterViewModel(application) as T
             modelClass.isAssignableFrom(ImageViewModel::class.java) -> ImageViewModel(application) as T
             else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
