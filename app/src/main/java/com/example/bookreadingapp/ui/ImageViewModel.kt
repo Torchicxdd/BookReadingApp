7e86213a78e46d01fd30c314c9c0272f82f9556f
David-Parkerr <davidparker0040@gmail.com>
2024-11-17 02:56:27 -0500

    Create ImageViewModel to connect db to ui

---
+++ app/src/main/java/com/example/bookreadingapp/ui/ImageViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.bookreadingapp.ui
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bookreadingapp.data.BooksAppRoomDatabase
+import com.example.bookreadingapp.data.entities.Image
+import com.example.bookreadingapp.data.repositories.ImageRepository
+
+class ImageViewModel(application: Application) : ViewModel() {
+    val searchedResults: MutableLiveData<List<Image>>
+    private val repository: ImageRepository
+
+    init {
+        val imageDb = BooksAppRoomDatabase.getInstance(application)
+        val imageDao = imageDb.imageDao()
+        repository = ImageRepository(imageDao)
+
+        searchedResults = repository.searchResults
+    }
+}
