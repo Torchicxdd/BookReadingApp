647981e1e2fed996c8001d05735774f217d57c00
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-16 20:16:03 -0500

    Made Room Database

---
+++ app/src/main/java/com/example/bookreadingapp/data/BooksAppRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.bookreadingapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.bookreadingapp.data.daos.BooksDao
+import com.example.bookreadingapp.data.entities.Books
+
+@Database(entities = [(Books::class)], version = 1)
+abstract class BooksAppRoomDatabase: RoomDatabase() {
+    abstract fun booksDao(): BooksDao
+
+    companion object {
+        private var INSTANCE: BooksAppRoomDatabase? = null
+        fun getInstance(context: Context): BooksAppRoomDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        BooksAppRoomDatabase::class.java,
+                        "books_app_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
