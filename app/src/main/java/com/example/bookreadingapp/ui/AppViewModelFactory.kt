98e99bddd6ce1993eb65fa2ca8362bcdefc05dc9
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-17 22:26:59 -0500

    Fix rebase conflicts

--- app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
@@ -9,16 +9,9 @@ class AppViewModelFactory(val application: Application): ViewModelProvider.Facto
         return when {
             modelClass.isAssignableFrom(BookViewModel::class.java) -> BookViewModel(application) as T
             modelClass.isAssignableFrom(ChapterViewModel::class.java) -> ChapterViewModel(application) as T
-<<<<<<< HEAD
             modelClass.isAssignableFrom(ImageViewModel::class.java) -> ImageViewModel(application) as T
             modelClass.isAssignableFrom(ParagraphViewModel::class.java) -> ParagraphViewModel(application) as T
             else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
 }
-=======
-            else -> throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
-}
->>>>>>> f17a6df (Add chapters view model)
