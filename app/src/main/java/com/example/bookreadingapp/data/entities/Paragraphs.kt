1456892acaa334e6c0f06cc55c59102d3aaf2450
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-17 19:06:01 -0500

    Made Pharagraphs entity

---
+++ app/src/main/java/com/example/bookreadingapp/data/entities/Paragraphs.kt
@@ -0,0 +1,31 @@
+package com.example.bookreadingapp.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "paragraphs",
+    foreignKeys = [
+        ForeignKey(
+            entity = Chapters::class,
+            parentColumns = arrayOf("chapterId"),
+            childColumns = arrayOf("chapterId"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+class Paragraphs {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "paragraphId")
+    var id: Int = 0
+    var chapterId: Int = 0
+    var text: String = ""
+
+    constructor()
+    constructor(text: String) {
+        this.text = text
+    }
+}
