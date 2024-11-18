714d4ecc0124089bbc188d48b6e25b3cf2e70917
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-17 19:23:37 -0500

    Made the ViewModel for the Paragraphs entity

--- app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/AppViewModelFactory.kt
@@ -11,6 +11,7 @@ class AppViewModelFactory(val application: Application): ViewModelProvider.Facto
             modelClass.isAssignableFrom(ChapterViewModel::class.java) -> ChapterViewModel(application) as T
 <<<<<<< HEAD
             modelClass.isAssignableFrom(ImageViewModel::class.java) -> ImageViewModel(application) as T
+            modelClass.isAssignableFrom(ParagraphViewModel::class.java) -> ParagraphViewModel(application) as T
             else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
---
+++ app/src/main/java/com/example/bookreadingapp/ui/ParagraphViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.bookreadingapp.ui
+
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bookreadingapp.data.BooksAppRoomDatabase
+import com.example.bookreadingapp.data.entities.Image
+import com.example.bookreadingapp.data.entities.Paragraphs
+import com.example.bookreadingapp.data.repositories.ParagraphsRepository
+
+class ParagraphViewModel(application: Application) : ViewModel() {
+    val searchedResults: MutableLiveData<List<Paragraphs>>
+    private val repository: ParagraphsRepository
+
+    init {
+        val paragraphDb = BooksAppRoomDatabase.getInstance(application)
+        val paragraphDao = paragraphDb.paragraphDao()
+        repository = ParagraphsRepository(paragraphDao)
+
+        searchedResults = repository.searchResults
+    }
+
+    fun insertParagraph(paragraph: Paragraphs) {
+        repository.insertParagraph(paragraph)
+    }
+
+    fun deleteParagraph(id: Int) {
+        repository.deleteParagraph(id)
+    }
+
+    fun findParagraphById(id: Int) {
+        repository.findParagraphById(id)
+    }
+
+    fun findParagraphByChapterId(chapterId: Int) {
+        repository.findParagraphByChapterId(chapterId)
+    }
+}
