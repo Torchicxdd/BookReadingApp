86301853d7e3c6ca2d816b679f4ec04228501698
elianasmadja <eliana.smadja@dawsoncollege.qc.ca>
2024-11-17 19:59:45 -0500

    fix navigation tests for updated nav

--- app/src/androidTest/java/com/example/bookreadingapp/NavUiTests.kt
+++ app/src/androidTest/java/com/example/bookreadingapp/NavUiTests.kt
@@ -1,45 +1,12 @@
 package com.example.bookreadingapp
-import androidx.test.espresso.base.Default
-import com.example.bookreadingapp.MainActivity
-import com.example.bookreadingapp.objects.BottomNavBar
-import com.example.bookreadingapp.objects.PermanentNavDrawer
-import com.example.bookreadingapp.objects.TopAppBar
-
-
-import android.content.Context
-import androidx.activity.compose.setContent
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.isDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.Navigation
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import com.example.bookreadingapp.objects.AppViewModel
-import com.example.bookreadingapp.objects.NavigationHost
-import com.example.bookreadingapp.objects.Routes
-import com.example.bookreadingapp.screens.ContentTable
-import com.example.bookreadingapp.screens.Home
-import com.example.bookreadingapp.screens.Library
-import com.example.bookreadingapp.screens.Reading
-import com.example.bookreadingapp.screens.Search
 import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
-import com.example.bookreadingapp.utils.AdaptiveNavigationType
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -64,39 +31,28 @@ class NavUiTests {
         }
     }
 
-
+    //tests that all navigation buttons are visible
     @Test
     fun testBottomNavigationIsVisible() {
         // Wait for idle state to ensure UI is rendered
         composeTestRule.waitForIdle()
 
-        // Assert that the Home button is visible
         composeTestRule.onNodeWithTag("home_button").assertIsDisplayed()
-
-        // Assert that the Library button is visible
         composeTestRule.onNodeWithTag("library_button").assertIsDisplayed()
-
-        // Assert that the Search button is visible
-        composeTestRule.onNodeWithTag("search_button").assertIsDisplayed()
-
-        // Assert that the Content Table button is visible
-        composeTestRule.onNodeWithTag("content_button").assertIsDisplayed()
-
-        // Assert that the Reading button is visible
-        composeTestRule.onNodeWithTag("reading_button").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("bookshelf_button").assertIsDisplayed()
     }
 
+    //test that home screen is visible on startup
     @Test
     fun testInitialNavigationToHomeScreen() {
 
         composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
     }
 
+    //test that library button click brings to library screen
     @Test
     fun testNavigateToLibraryScreen() {
 
-
-
         composeTestRule.waitForIdle()
 
         composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
@@ -106,173 +62,94 @@ class NavUiTests {
         composeTestRule.onNodeWithTag("library_screen").assertIsDisplayed()
     }
 
-
-
+    //test that bookshelf is empty initially
     @Test
-    fun testNavigateToReadingScreenFromLibrary() {
-
+    fun testNavigateToEmptyBookShelfScreen() {
+        composeTestRule.waitForIdle()
 
         composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("library_button").performClick()
-        composeTestRule.onNodeWithTag("book_item_2131689622").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("book_item_2131689622").performClick()
+
+        composeTestRule.onNodeWithTag("bookshelf_button").performClick()
+
+        composeTestRule.onNodeWithTag("no_books_screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("empty_bookshelf_text").assertIsDisplayed()
+
+    }
+
+//test that clicking book in library downloads it
+    @Test
+    fun testLibraryScreenBookClicking() {
 
         composeTestRule.waitForIdle()
 
+        composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("library_button").performClick()
+        composeTestRule.onNodeWithTag("library_screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("book_item_2131755140").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("book_item_2131755140").performClick()
+        composeTestRule.onNodeWithTag("book_item_2131755140").assertIsNotDisplayed()
+
+    }
+
+//test that downloaded book is displayed on bookshelf
+    @Test
+    fun testNavigateToBookShelfScreenWithBook() {
+        composeTestRule.waitForIdle()
+        testLibraryScreenBookClicking()
+        composeTestRule.onNodeWithTag("bookshelf_button").performClick()
+        composeTestRule.onNodeWithTag("bookshelf_screen").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("book_item_2131755140").assertIsDisplayed()
+    }
+
+    //test that clicking bookshelf book navigates to table of contents screen
+    @Test
+    fun testNavigateToTableOfContentScreenOnBookClick() {
+        composeTestRule.waitForIdle()
+        testNavigateToBookShelfScreenWithBook()
+        composeTestRule.onNodeWithTag("book_item_2131755140").performClick()
+        composeTestRule.onNodeWithTag("content_screen").assertIsDisplayed()
+    }
+
+
+
+//test that reading button navigates to reading screen
+    @Test
+    fun testNavigateToReadingScreenFromTableContent() {
+        testNavigateToTableOfContentScreenOnBookClick()
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithTag("go_to_reading_button").performClick()
         composeTestRule.onNodeWithTag("reading_screen").assertIsDisplayed()
     }
 
+//test that reading mode works and removes navbar
+    @Test
+    fun testReadingModeButton() {
+        testNavigateToReadingScreenFromTableContent()
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithTag("home_button").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("library_button").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("bookshelf_button").assertIsDisplayed()
+
+        composeTestRule.onNodeWithTag("reading_mode_button").performClick()
+
+        composeTestRule.onNodeWithTag("home_button").assertIsNotDisplayed()
+        composeTestRule.onNodeWithTag("library_button").assertIsNotDisplayed()
+        composeTestRule.onNodeWithTag("bookshelf_button").assertIsNotDisplayed()
+
+    }
+
+
+//test that search button navigates to search screen
     @Test
     fun testNavigateToSearchScreen() {
-
-
-
+        testNavigateToTableOfContentScreenOnBookClick()
         composeTestRule.waitForIdle()
 
-        composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("search_button").performClick()
+        composeTestRule.onNodeWithTag("go_to_search_button").performClick()
 
         composeTestRule.onNodeWithTag("search_screen").assertIsDisplayed()
     }
-
-    @Test
-    fun testNavigateToContentTableScreen() {
-
-
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("content_button").performClick()
-
-        composeTestRule.onNodeWithTag("content_screen").assertIsDisplayed()
-    }
-
-    @Test
-    fun testNavigateToReadingScreen() {
-
-
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("reading_button").performClick()
-
-        composeTestRule.onNodeWithTag("reading_screen").assertIsDisplayed()
-    }
-
-
-
-
-
-
-    @Test
-    fun testLibraryScreenDisplaysCorrectly() {
-
-        composeTestRule.waitForIdle()
-        composeTestRule.onNodeWithTag("home_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("library_button").performClick()
-        composeTestRule.onNodeWithTag("library_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("book_item_2131689622").assertIsDisplayed()
-
-    }
-
-
-    @Test
-    fun testViewModelStateChangeOnButtonClickInHome() {
-
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This is the state before being changed")
-
-        composeTestRule.onNodeWithTag("home_viewmodel_button").performClick()
-
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This state was changed from the Home screen")
-
-        composeTestRule.onNodeWithTag("library_button").performClick()
-        composeTestRule.onNodeWithTag("library_viewmodel_text")
-            .assertTextEquals("This state was changed from the Home screen")
-
-
-    }
-
-    @Test
-    fun testViewModelStateChangeOnButtonClickInLibrary() {
-
-        composeTestRule.onNodeWithTag("library_button").performClick()
-        composeTestRule.onNodeWithTag("library_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("library_viewmodel_text")
-            .assertTextEquals("This is the state before being changed")
-
-        composeTestRule.onNodeWithTag("library_viewmodel_button").performClick()
-
-        composeTestRule.onNodeWithTag("library_viewmodel_text")
-            .assertTextEquals("This state was changed from the Library screen")
-
-        composeTestRule.onNodeWithTag("home_button").performClick()
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This state was changed from the Library screen")
-    }
-
-    @Test
-    fun testViewModelStateChangeOnButtonClickInReading() {
-
-        composeTestRule.onNodeWithTag("reading_button").performClick()
-        composeTestRule.onNodeWithTag("reading_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("reading_viewmodel_text")
-            .assertTextEquals("This is the state before being changed")
-
-        composeTestRule.onNodeWithTag("reading_viewmodel_button").performClick()
-
-        composeTestRule.onNodeWithTag("reading_viewmodel_text")
-            .assertTextEquals("This state was changed from the Reading screen")
-
-        composeTestRule.onNodeWithTag("home_button").performClick()
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This state was changed from the Reading screen")
-    }
-
-    @Test
-    fun testViewModelStateChangeOnButtonClickInSearch() {
-
-        composeTestRule.onNodeWithTag("search_button").performClick()
-        composeTestRule.onNodeWithTag("search_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("search_viewmodel_text")
-            .assertTextEquals("This is the state before being changed")
-
-        composeTestRule.onNodeWithTag("search_viewmodel_button").performClick()
-
-        composeTestRule.onNodeWithTag("search_viewmodel_text")
-            .assertTextEquals("This state was changed from the Search screen")
-
-        composeTestRule.onNodeWithTag("home_button").performClick()
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This state was changed from the Search screen")
-    }
-
-    @Test
-    fun testViewModelStateChangeOnButtonClickInContentTable() {
-
-        composeTestRule.onNodeWithTag("content_button").performClick()
-        composeTestRule.onNodeWithTag("content_screen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("content_viewmodel_text")
-            .assertTextEquals("This is the state before being changed")
-
-        composeTestRule.onNodeWithTag("content_viewmodel_button").performClick()
-
-        composeTestRule.onNodeWithTag("content_viewmodel_text")
-            .assertTextEquals("This state was changed from the Content screen")
-
-        composeTestRule.onNodeWithTag("home_button").performClick()
-        composeTestRule.onNodeWithTag("home_viewmodel_text")
-            .assertTextEquals("This state was changed from the Content screen")
-    }
-
-
-
 
 }
-
