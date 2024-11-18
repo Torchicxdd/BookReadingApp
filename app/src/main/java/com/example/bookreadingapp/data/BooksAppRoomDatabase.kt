5fc78fb91ca2aa9dd1a274197839f131b353f62f
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-17 19:23:37 -0500

    Made the ViewModel for the Paragraphs entity

--- app/src/main/java/com/example/bookreadingapp/data/BooksAppRoomDatabase.kt
+++ app/src/main/java/com/example/bookreadingapp/data/BooksAppRoomDatabase.kt
@@ -7,15 +7,18 @@ import androidx.room.RoomDatabase
 import com.example.bookreadingapp.data.daos.BooksDao
 import com.example.bookreadingapp.data.daos.ChaptersDao
 import com.example.bookreadingapp.data.daos.ImageDao
+import com.example.bookreadingapp.data.daos.ParagraphDao
 import com.example.bookreadingapp.data.entities.Books
 import com.example.bookreadingapp.data.entities.Chapters
 import com.example.bookreadingapp.data.entities.Image
+import com.example.bookreadingapp.data.entities.Paragraphs
 
-@Database(entities = [Books::class, Image::class, Chapters::class], version = 1)
+@Database(entities = [Books::class, Image::class, Chapters::class, Paragraphs::class], version = 1)
 abstract class BooksAppRoomDatabase: RoomDatabase() {
     abstract fun booksDao(): BooksDao
     abstract fun chaptersDao(): ChaptersDao
     abstract fun imageDao(): ImageDao
+    abstract fun paragraphDao(): ParagraphDao
 
 
     companion object {
