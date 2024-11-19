960a3bec17e846ebd8883b0246580ab32698822e
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-18 19:20:51 -0500

    Fix conflicts

--- app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
+++ app/src/main/java/com/example/bookreadingapp/data/entities/Books.kt
@@ -20,7 +20,6 @@ class Books {
 
     constructor()
     constructor(title: String, author: String, coverImage: String) {
-
         this.title = title
         this.author = author
         this.coverImage = coverImage
