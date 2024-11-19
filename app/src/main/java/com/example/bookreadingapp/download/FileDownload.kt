24cd58e0427fe59605d937146ea25d88c7f57a19
HaiHoang_Do <hoang.do@dawsoncollege.qc.ca>
2024-11-18 21:30:22 -0800

    Run IO operations in FileDownload class in IO Dispatcher

--- app/src/main/java/com/example/bookreadingapp/download/FileDownload.kt
+++ app/src/main/java/com/example/bookreadingapp/download/FileDownload.kt
@@ -3,8 +3,8 @@ package com.example.bookreadingapp.download
 import android.content.Context
 import android.os.Environment
 import android.util.Log
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.BufferedOutputStream
@@ -13,18 +13,13 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.util.zip.ZipFile
-import kotlinx.coroutines.channels.*
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 private const val TAG_FE = "FileExtract"
 
 class FileDownload(private val context: Context) {
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
-
     // Create download folder if it doesn't exist, then saves file
     suspend fun createFile(directoryName: String, fileName: String): File {
-        var downloadFolder: File = File("")
+        var downloadFolder: File
         withContext(Dispatchers.IO) {
             downloadFolder = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
             if (!downloadFolder.exists()) downloadFolder.mkdirs()
@@ -35,7 +30,7 @@ class FileDownload(private val context: Context) {
 
     // List directory contents
     suspend fun listDirectoryContents(directoryName: String): List<String> {
-        var folderToRead: File = File("")
+        var folderToRead: File
         withContext(Dispatchers.IO) {
             folderToRead = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
         }
@@ -45,28 +40,29 @@ class FileDownload(private val context: Context) {
 
     // Request content from url and saves to file location
     suspend fun downloadFile(url: String, file: File): Boolean {
-        coroutineScope.launch(Dispatchers.Main) {
+        var downloadSuccess : Boolean
+        withContext(Dispatchers.IO) {
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder().url(url).build()
+                val response = client.newCall(request).execute()
 
-        }
-        try {
-            val client = OkHttpClient()
-            val request = Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-
-            // Returns false if network request was unsuccessful
-            if (!response.isSuccessful || response.body == null) {
-                return false
-            }
-            response.body!!.byteStream().use { inputStream ->
-                FileOutputStream(file).use { outputStream ->
-                    copyData(inputStream, outputStream)
+                // Returns false if network request was unsuccessful
+                if (!response.isSuccessful || response.body == null) {
+                    downloadSuccess = false
                 }
+                response.body!!.byteStream().use { inputStream ->
+                    FileOutputStream(file).use { outputStream ->
+                        copyData(inputStream, outputStream)
+                    }
+                }
+                downloadSuccess = true
+            } catch(e: IOException) {
+                e.printStackTrace()
+                downloadSuccess = false
             }
-            return true
-        } catch(e: IOException) {
-            e.printStackTrace()
-            return false
         }
+        return downloadSuccess
     }
 
     // Helper methods
@@ -81,45 +77,44 @@ class FileDownload(private val context: Context) {
 
     // Delete directory contents directly without IntentSender
     suspend fun deleteDirectoryContents(directoryName: String) {
-        coroutineScope.launch(Dispatchers.Main) {
-
-        }
-        val downloadFolder = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
-        downloadFolder.listFiles()?.forEach {
-            it.delete()
+        var folderToDelete : File
+        withContext(Dispatchers.IO) {
+            folderToDelete = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
+            folderToDelete.listFiles()?.forEach {
+                it.delete()
+            }
         }
     }
 
     // Unzip file and saves to the same directory
     // Returns absolute path of unzipped html file
     suspend fun unzipFile(zipFile: File, directoryName: String) : String {
-        coroutineScope.launch(Dispatchers.Main) {
-
-        }
         var unzippedPath = ""
-        val unzipFolder = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
-        if (!unzipFolder.exists()) {
-            unzipFolder.mkdirs()
-        }
-        // Log where unzipped content will be placed
-        Log.i(TAG_FE, unzipFolder.absolutePath)
+        withContext(Dispatchers.IO) {
+            val unzipFolder = File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), directoryName)
+            if (!unzipFolder.exists()) {
+                unzipFolder.mkdirs()
+            }
+            // Log where unzipped content will be placed
+            Log.i(TAG_FE, unzipFolder.absolutePath)
 
-        ZipFile(zipFile).use { zip ->
-            zip.entries().asSequence().forEach { entry ->
-                if(entry.name.contains(".html")) {
-                    zip.getInputStream(entry).use { input ->
-                        val filePath = unzipFolder.absolutePath + File.separator + entry.name
+            ZipFile(zipFile).use { zip ->
+                zip.entries().asSequence().forEach { entry ->
+                    if(entry.name.contains(".html")) {
+                        zip.getInputStream(entry).use { input ->
+                            val filePath = unzipFolder.absolutePath + File.separator + entry.name
 
-                        if (!entry.isDirectory) {
-                            extractFile(input, filePath)
-                        } else {
-                            val dir = File(filePath)
-                            dir.mkdir()
+                            if (!entry.isDirectory) {
+                                extractFile(input, filePath)
+                            } else {
+                                val dir = File(filePath)
+                                dir.mkdir()
+                            }
+                            // Need to change to also unzip images after
+                            // Log location of the unzipped html file
+                            Log.i(TAG_FE, File(filePath).absolutePath + " Exists: " + File(filePath).exists())
+                            unzippedPath = File(filePath).absolutePath
                         }
-                        // Need to change to also unzip images after
-                        // Log location of the unzipped html file
-                        Log.i(TAG_FE, File(filePath).absolutePath + " Exists: " + File(filePath).exists())
-                        unzippedPath = File(filePath).absolutePath
                     }
                 }
             }
