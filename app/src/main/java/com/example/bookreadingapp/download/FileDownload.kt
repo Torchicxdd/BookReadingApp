ba4110934c39b4febff4564af24dd266d219c362
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-19 22:29:23 -0500

    Made a test for the unzipping

--- app/src/main/java/com/example/bookreadingapp/download/FileDownload.kt
+++ app/src/main/java/com/example/bookreadingapp/download/FileDownload.kt
@@ -98,8 +98,6 @@ class FileDownload(private val context: Context) {
             if (!unzipFolder.exists()) {
                 unzipFolder.mkdirs()
             }
-            // Log where unzipped content will be placed
-//            Log.i(TAG_FE, unzipFolder.absolutePath)
 
             ZipFile(zipFile).use { zip ->
                 zip.entries().asSequence().forEach { entry ->
@@ -113,7 +111,7 @@ class FileDownload(private val context: Context) {
                             val dir = File(destFilePath)
                             dir.mkdir()
                         }
-                        Log.i(TAG_FE, entry.name)
+
                         if (entry.name.contains(".html")) {
                             unzippedPath = File(destFilePath).absolutePath
                         }
