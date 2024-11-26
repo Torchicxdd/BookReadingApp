262847690171bb71a9342b76be0ecb4480bb4c51
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-26 10:45:57 -0800

    Remove unused import

--- app/src/main/java/com/example/bookreadingapp/MainActivity.kt
+++ app/src/main/java/com/example/bookreadingapp/MainActivity.kt
@@ -1,38 +1,20 @@
 package com.example.bookreadingapp
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.example.bookreadingapp.ui.BookReadingApp
 import com.example.bookreadingapp.ui.viewmodels.DownloadViewModel
-import com.example.bookreadingapp.ui.viewmodels.AppViewModel
-import com.example.bookreadingapp.ui.objects.BottomNavBar
 import com.example.bookreadingapp.ui.viewmodels.DownloadViewModelFactory
-import com.example.bookreadingapp.ui.objects.NavRail
-import com.example.bookreadingapp.ui.objects.NavigationHost
-import com.example.bookreadingapp.ui.objects.PermanentNavDrawer
-import com.example.bookreadingapp.ui.objects.TopAppBar
 import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
-import com.example.bookreadingapp.ui.utils.AdaptiveNavigationType
 
 
 class MainActivity : ComponentActivity() {
