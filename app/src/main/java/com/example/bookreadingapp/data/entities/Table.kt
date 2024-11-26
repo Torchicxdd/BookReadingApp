3dbe6183906a667fbe35429bed3c09131c0be60d
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-26 10:29:07 -0800

    Made the class for the table entity

---
+++ app/src/main/java/com/example/bookreadingapp/data/entities/Table.kt
@@ -0,0 +1,31 @@
+package com.example.bookreadingapp.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "tables",
+    foreignKeys = [
+        ForeignKey(
+            entity = Chapters::class,
+            parentColumns = arrayOf("chapterId"),
+            childColumns = arrayOf("chapterId"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )]
+)
+class Table {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "tableId")
+    var id: Int = 0
+    var chapterId: Int = 0
+    var content: String = ""
+
+    constructor()
+    constructor(content: String, chapterId: Int) {
+        this.content = content
+        this.chapterId = chapterId
+    }
+}
