b7f4a828654773a3de1c5fc0a5cde1c475e6c07c
David-Parkerr <davidparker0040@gmail.com>
2024-11-26 12:42:46 -0500

    Pass new parameters to pass state down

--- app/src/main/java/com/example/bookreadingapp/ui/objects/Navigation.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/objects/Navigation.kt
@@ -66,22 +66,52 @@ fun NavigationHost(
         startDestination = Routes.Home.route
     ) {
         composable(Routes.Home.route) {
-            Home(context, viewModel, adaptiveNavigationType)
+            Home(context = context)
         }
         composable(Routes.Library.route) {
-            Library(context, viewModel, navController, adaptiveNavigationType, downloadViewModel)
+            Library(
+                context = context,
+                libraryBooks = viewModel.libraryBooks,
+                moveBookToBookshelf = { viewModel.moveBookToBookshelf(it) },
+                setupDownload = { url: String, dir: String -> downloadViewModel.setupDownload(url, dir) }
+            )
         }
         composable(Routes.Bookshelf.route) {
-            Bookshelf(context, viewModel, navController, adaptiveNavigationType)
+            Bookshelf(
+                context = context,
+                bookshelfBooks = viewModel.bookshelfBooks,
+                updateBookTitle = { viewModel.updateBookTitle(it) },
+                navigateToTableOfContents = { navController.navigate(Routes.ContentTable.route){
+                    launchSingleTop = true
+                    restoreState = true
+                } }
+            )
         }
         composable(Routes.Search.route) {
-            Search(context, viewModel, navController, adaptiveNavigationType)
+            Search(
+                context = context,
+                selectedBookTitle = viewModel.selectedBookTitleResId,
+                searchBarInput = viewModel.searchBarInput,
+                updateSearchBar = { viewModel.updateSearchBarInput(it) },
+                performSearch = { viewModel.performSearch() },
+                searchResult = viewModel.searchResultText
+            )
         }
         composable(Routes.ContentTable.route) {
-            ContentTable(context, viewModel, navController, adaptiveNavigationType)
+            ContentTable(
+                context = context,
+                selectedBookTitle = viewModel.selectedBookTitleResId,
+                navigateToSearch =  { navController.navigate(Routes.Search.route) },
+                navigateToReading =  { navController.navigate(Routes.Reading.route) }
+            )
         }
         composable(Routes.Reading.route) {
-            Reading(context, viewModel, navController, adaptiveNavigationType)
+            Reading(
+                context = context,
+                bookTitle =  viewModel.selectedBookTitleResId,
+                isInReadingMode =  viewModel.readingMode,
+                toggleReadingMode =  { viewModel.updateReadingMode() }
+            )
         }
 
     }
