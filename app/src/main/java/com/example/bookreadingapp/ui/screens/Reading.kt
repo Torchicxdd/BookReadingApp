c7837f6834af7672dc4972a3cdc17449dbd00fa3
maara <maara.purici@dawsoncollege.qc.ca>
2024-11-29 21:10:24 -0500

    Fix conflicts

--- app/src/main/java/com/example/bookreadingapp/ui/screens/Reading.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/screens/Reading.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,6 +60,7 @@ fun Reading(
                 titleResourceId = book.title
             )
         }
+        ChapterNavigation()
     }
 }
 
@@ -80,5 +84,32 @@ fun BookDisplay(
             textAlign = TextAlign.Center,
             modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_medium))
         )
+    }
+}
+
+/**
+ * The button used to navigate through a chapter
+ */
+@Composable
+fun ChapterNavigation() {
+    Row(
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Button(
+           onClick = {/** TO DO: Go back to previous chapter (page?) */}
+        ){
+            Text(
+                text="<"
+            )
+        }
+
+        Button(
+            onClick = {/** TO DO: Go back to next chapter (page?) */}
+        ){
+            Text(
+                text=">"
+            )
+        }
     }
 }
