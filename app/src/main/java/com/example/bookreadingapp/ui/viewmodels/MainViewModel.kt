148f1d9dfe536d2a4f91d759f695d6204085f5e9
HaiHoang_Do <hoang.do@dawsoncollege.qc.ca>
2024-12-01 18:19:27 -0800

    Create mainViewModel to hold other viewmodels

---
+++ app/src/main/java/com/example/bookreadingapp/ui/viewmodels/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bookreadingapp.ui.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    val bookViewModel: BookViewModel = BookViewModel(application)
+}
