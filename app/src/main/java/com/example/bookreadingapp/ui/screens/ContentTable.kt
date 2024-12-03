4fef210a47fffc1e421127a3e1e394055b22f18e
HaiHoang_Do <hoang.do@dawsoncollege.qc.ca>
2024-12-02 19:46:43 -0800

    Display chapter list in table of content

--- app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
@@ -1,5 +1,6 @@
 package com.example.bookreadingapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,9 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -18,9 +22,13 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.bookreadingapp.R
 import com.example.bookreadingapp.data.Book
+import com.example.bookreadingapp.data.entities.Chapters
 import com.example.bookreadingapp.ui.utils.GoToReadingButton
 import com.example.bookreadingapp.ui.utils.GoToSearchButton
 import com.example.bookreadingapp.ui.viewmodels.MainViewModel
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 /**
  * Table of contents page
@@ -34,7 +42,14 @@ fun ContentTable(
     navigateToSearch: () -> Unit,
     navigateToReading: () -> Unit,
 ) {
-//    mainViewModel.chapterViewModel.getChaptersByBookId()
+    // Observe search results in viewmodel
+    val searchChapterResults by mainViewModel.chapterViewModel.allChapters.observeAsState(listOf())
+    val chapterViewModel = mainViewModel.chapterViewModel
+    if (book != null) {
+        Log.i("ContentTable", "Not Null - ${book.bookID}")
+        chapterViewModel.getChaptersByBookId(book.bookID)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -66,6 +81,23 @@ fun ContentTable(
                 GoToSearchButton(navigateToSearch)
                 GoToReadingButton(navigateToReading)
             }
+            
+            DisplayChaptersList(chapters = searchChapterResults)
         }
     }
+}
+
+@Composable
+fun DisplayChaptersList(
+    chapters: List<Chapters>
+) {
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxSize(),
+        content = {
+            items(chapters) { chapter ->
+                Text(chapter.title)
+            }
+        }
+    )
 }
