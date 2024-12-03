d435c0649ac54dc66faff33431b5e558d4f62fb5
David-Parkerr <davidparker0040@gmail.com>
2024-12-03 15:42:00 -0500

    Refactor table of content to have a main composable

--- app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
@@ -1,51 +1,37 @@
 package com.example.bookreadingapp.ui.screens
 
-import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.example.bookreadingapp.R
 import com.example.bookreadingapp.data.Book
 import com.example.bookreadingapp.data.entities.Chapters
 import com.example.bookreadingapp.ui.utils.GoToReadingButton
 import com.example.bookreadingapp.ui.utils.GoToSearchButton
-import com.example.bookreadingapp.ui.viewmodels.MainViewModel
-import androidx.compose.runtime.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookreadingapp.ui.viewmodels.AppViewModel
-import kotlinx.coroutines.launch
+import com.example.bookreadingapp.ui.viewmodels.MainViewModel
 
 /**
  * Table of contents page
@@ -70,17 +56,34 @@ fun ContentTable(
         }
     }
 
+    TableOfContentMain(
+        book = book,
+        navigateToSearch = navigateToSearch,
+        navigateToReading = navigateToReading,
+        searchChapterResults = searchChapterResults
+    )
+}
+
+@Composable
+fun TableOfContentMain(
+    book: Book?,
+    navigateToSearch: () -> Unit,
+    navigateToReading: (Long) -> Unit,
+    searchChapterResults: List<Chapters>
+) {
+    val verticalScrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .testTag("content_screen")
+            .verticalScroll(verticalScrollState)
     ) {
         // Content section
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
         ) {
             // Title and selected book information
             TableOfContentsHeader(
@@ -89,7 +92,7 @@ fun ContentTable(
                 navigateToSearch = navigateToSearch,
                 navigateToReading = navigateToReading
             )
-            
+
             DisplayChaptersList(chapters = searchChapterResults, navigateToReading = navigateToReading)
         }
     }
@@ -138,37 +141,36 @@ fun DisplayChaptersList(
            text = stringResource(R.string.num_chap, chapters.size),
            fontWeight = FontWeight.Bold,
            modifier = Modifier
-               .padding(top = dimensionResource(R.dimen.padding_medium))
+               .padding(
+                   top = dimensionResource(R.dimen.padding_medium),
+                   start = dimensionResource(R.dimen.padding_medium)
+               )
        )
-       LazyColumn (
-           modifier = Modifier
-               .fillMaxSize(),
-           content = {
-               items(chapters) { chapter ->
-                   DisplayChapter(chapter = chapter, navigateToReading = navigateToReading)
-               }
-           }
-       )
+       for ((i, chapter) in chapters.withIndex()) {
+           DisplayChapter(
+               chapter = chapter,
+               chapterNumber = i,
+               navigateToReading = navigateToReading
+           )
+       }
    }
 }
 
 @Composable
 fun DisplayChapter(
     chapter: Chapters,
+    chapterNumber: Int,
     navigateToReading: (Long) -> Unit
 ) {
-    Text(
-        text = chapter.title,
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(
-                top = dimensionResource(R.dimen.padding_small),
-                bottom = dimensionResource(R.dimen.padding_small)
-            )
-            .clickable(
-                onClick = {
-                    navigateToReading(chapter.id)
-                }
-            )
-    )
+            .clickable { navigateToReading(chapter.id) }
+            .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        Text(
+            text = stringResource(R.string.name_chap, chapterNumber, chapter.title),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
