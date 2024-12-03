6f5e35a3b8c05cd864ccc7c70490d441d34ec6bb
David-Parkerr <davidparker0040@gmail.com>
2024-12-02 22:52:53 -0500

    Make table of content image smaller

--- app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/screens/ContentTable.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -104,7 +106,7 @@ fun TableOfContentsHeader(
             painter = painterResource(bookCover),
             contentDescription = null,
             modifier = Modifier
-                .size(dimensionResource(R.dimen.image_size))
+                .size(dimensionResource(R.dimen.small_image))
         )
         Column(
             verticalArrangement = Arrangement.SpaceBetween
