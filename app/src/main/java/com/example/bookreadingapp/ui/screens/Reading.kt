ea9732b57eac10e65c1711eb1e2ec38fa043234f
HaiHoang_Do <hoang.do@dawsoncollege.qc.ca>
2024-12-03 18:50:39 -0800

    Rebase from dev

--- app/src/main/java/com/example/bookreadingapp/ui/screens/Reading.kt
+++ app/src/main/java/com/example/bookreadingapp/ui/screens/Reading.kt
@@ -1,60 +1,47 @@
 package com.example.bookreadingapp.ui.screens
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextMeasurer
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.bookreadingapp.R
 import com.example.bookreadingapp.data.Book
 import com.example.bookreadingapp.data.entities.Image
 import com.example.bookreadingapp.data.entities.Paragraphs
 import com.example.bookreadingapp.data.entities.Table
-import com.example.bookreadingapp.ui.utils.BookCover
 import com.example.bookreadingapp.ui.viewmodels.AppViewModel
 import com.example.bookreadingapp.ui.viewmodels.MainViewModel
+import java.io.File
 
 
 /**
@@ -84,7 +71,6 @@ fun Reading(
     val searchImagesResult by mainViewModel.imageViewModel.searchedResults.observeAsState(listOf())
 
     val stringList = createStringList(searchParagraphsResult, searchTablesResult, searchImagesResult)
-    var imagePath = ""
 
     BoxWithConstraints(
         modifier = Modifier
@@ -176,6 +162,24 @@ fun PageDisplay(
                 modifier = Modifier.fillMaxWidth()
             )
         }
+    }
+}
+/**
+ * Load and display an image from app's internal storage
+ */
+@Composable
+fun StorageImage(
+    parentDirectory: String?,
+    imageName: String
+) {
+    val imagePath = parentDirectory + imageName
+    val imageFile = File(imagePath)
+
+    if (imageFile.exists()) {
+        val bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+
+        Image(bitmap = bitmap.asImageBitmap(),
+            contentDescription = "/images/fig70tn.gif")
     }
 }
 
